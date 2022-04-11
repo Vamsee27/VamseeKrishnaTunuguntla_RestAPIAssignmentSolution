0faa068888fa717cb9782e110be1ac08b7009cad
Vamsee Krishna Tunuguntla <54145589+Vamsee27@users.noreply.github.com>
2022-04-11 09:44:52 -0400

    Create EmployeeserviceDemoApplication.java

---
+++ src/main/java/com.example.empservice/EmployeeserviceDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.empservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeserviceDemoApplication.class, args);
+
+		System.out.println("Hello Springboot");
+	}
+
+}
