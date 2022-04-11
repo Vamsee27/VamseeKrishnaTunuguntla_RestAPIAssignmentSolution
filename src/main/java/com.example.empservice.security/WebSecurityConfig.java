0834c222dcc187b5b9bf018c770de0e6c965a86a
Vamsee Krishna Tunuguntla <54145589+Vamsee27@users.noreply.github.com>
2022-04-11 09:52:07 -0400

    Create WebSecurityConfig.java

---
+++ src/main/java/com.example.empservice.security/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.empservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.empservice.service.UserDetailsServiceImpl;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsServiceImpl();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/h2-console/").permitAll().antMatchers("/api/user", "/api/role")
+				.hasAuthority("ADMIN").antMatchers(HttpMethod.POST, "/api/employees").hasAuthority("ADMIN")
+				.antMatchers(HttpMethod.PUT, "/api/employees").hasAuthority("ADMIN")
+				.antMatchers(HttpMethod.DELETE, "/api/employees/*").hasAuthority("ADMIN")
+				// .antMatchers("/","/employee/save","/employee/showFormForAdd","/employee/403").hasAnyAuthority("USER","ADMIN")
+				// .antMatchers("/employee/showFormForUpdate","/employee/delete").hasAuthority("ADMIN")
+				.anyRequest().authenticated().and().httpBasic()
+				// .formLogin().loginProcessingUrl("/h2-console").successForwardUrl("/employee/list").permitAll()
+				// .and()
+				// .logout().logoutSuccessUrl("/h2-console").permitAll()
+				// .and()
+				// .exceptionHandling().accessDeniedPage("/employee/403")
+				.and().cors().and().csrf().disable();
+	}
+
+}
